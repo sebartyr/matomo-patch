74b9b7b7d4f29ee4040d06e46ac5d7618e15a557
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-08-14 17:58:30 +0200

    test patch

--- enable_plugin_upload.sh
+++ enable_plugin_upload.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 set -x 
 
-which php
-
 rm -rf plugins
 ln -s plugins-custom plugins
 
 clevercloud/pre_run.sh
 
-pwd
-
 sed -i '/\[General\]/a enable_plugin_upload=1' config/config.ini.php
+
+php console plugin:activate TagManager
+php console plugin:activate Bandwidth
