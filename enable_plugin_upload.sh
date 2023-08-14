e44f48d7390c0ab90590ec73cc52b2ed5eee09d8
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2023-08-14 16:35:03 +0200

    test patch

--- enable_plugin_upload.sh
+++ enable_plugin_upload.sh
@@ -10,6 +10,6 @@ clevercloud/pre_run.sh
 
 cp config/config.ini.php config/config.ini.php.back
 
-sed -i 's/[General]/[General]\nenable_plugin_upload=1/g' config/config.ini.php
+sed -i 's/[General]/[General]\nenable_plugin_upload=1/' config/config.ini.php
 
 rm config/config.ini.php.back
